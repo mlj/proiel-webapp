fffc5033b90539abb7899ecc86b5dfef169f347b
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2008-05-02 10:18:12 +0200

    Added new environment production-test.

---
+++ config/environments/production-test.rb
@@ -0,0 +1,27 @@
+# Settings specified here will take precedence over those in config/environment.rb
+config.active_record.observers = :user_observer
+
+# The production environment is meant for finished, "live" apps.
+# Code is not reloaded between requests
+config.cache_classes = true
+
+# Use a different logger for distributed setups
+# config.logger = SyslogLogger.new
+
+# Full error reports are disabled and caching is turned on
+config.action_controller.consider_all_requests_local = false
+config.action_controller.perform_caching             = true
+
+# Enable serving of images, stylesheets, and javascripts from an asset server
+# config.action_controller.asset_host                  = "http://assets.example.com"
+
+# Disable delivery errors, bad email addresses will be ignored
+# config.action_mailer.raise_delivery_errors = false
+config.action_mailer.delivery_method = :sendmail
+
+config.action_view.cache_template_loading = true
+
+ExceptionNotifier.exception_recipients = %w(mariuslj@ifi.uio.no)
+
+SITE_CANONICAL_URL = 'http://logos.uio.no:3001'
+SITE_ADMINISTRATOR_EMAIL = 'mariuslj@ifi.uio.no'
