0fec48a965c57e520c9e33624057b2522f5616ba
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2008-05-05 14:55:02 +0200

    Replaced flash message and validation message system with uniformly CSS styled message blocks.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -549,4 +549,41 @@ module ApplicationHelper
   def show_link_to_for_role(role, *args)
     show_link_to_if(current_user.has_role?(role), *args)
   end
+
+  # Outputs an error messages block.
+  #
+  # ==== Options
+  # * :on => :products   Also includes AR validation errors for @products
+  # * :clear => true     Clears messages after displaying
+  # * :keep => true      Keeps around messages for next response cycle
+  #
+  def message_block(options = {})
+    options.reverse_merge! :keep => false, :clear => true
+    out = ""
+
+    [:error, :notice].each do |type|
+      next if flash[type].nil? or flash[type].empty?
+      flash[type] = [flash[type]] unless flash[type].is_a?(Array)
+
+      out << "<div class=\"message-block #{type}\"><ul>\n"
+      flash[type].each {|msg| out << "<li>#{h(msg.to_s)}</li>\n"}
+      out << "</ul></div>\n"
+
+      flash[type] = nil if options[:clear]
+      flash.keep[type] if options[:keep]
+    end
+
+    if options[:on]
+      options[:on] = [options[:on]] unless options[:on].kind_of?(Array)
+      models = options[:on].map { |m| instance_variable_get("@" + m.to_s) }.select { |m| !m.nil? }
+      errors = models.inject([]) { |b, m| b += m.errors.full_messages }  
+
+      if errors.size > 0
+        out << "<div class=\"message-block error\"><ul>\n"
+        errors.each {|msg| out << "<li>#{h(msg.to_s)}</li>\n"}
+        out << "</ul></div>\n"
+      end
+    end
+    content_tag(:div, out, :class => 'flash')
+  end
 end
