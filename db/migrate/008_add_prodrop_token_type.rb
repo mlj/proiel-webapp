cf267ae0ef450a985c9e9d493e7800b9a5dccabd
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2008-08-01 17:12:31 +0200

    Fixed botched up migration.

--- db/migrate/008_add_prodrop_token_type.rb
+++ db/migrate/008_add_prodrop_token_type.rb
@@ -1,9 +1,9 @@
 class AddProdropTokenType < ActiveRecord::Migration
   def self.up
-    change_column :tokens, :sort, :enum, :limit => [:text, :punctuation, :empty_dependency_token, :lacuna_start, :lacuna_end, :word, :empty, :fused_morpheme, :enclitic, :nonspacing_punctuation, :spacing_punctuation, :left_bracketing_punctuation, :right_bracketing_punctuation, :prodrop], :default     => :word, :null => false
+    change_column :tokens, :sort, :enum, :limit => [:text, :punctuation, :empty_dependency_token, :lacuna_start, :lacuna_end, :prodrop], :default => :text, :null => false
   end
 
   def self.down
-    change_column :tokens, :sort, :enum, :limit => [:text, :punctuation, :empty_dependency_token, :lacuna_start, :lacuna_end, :word, :empty, :fused_morpheme, :enclitic, :nonspacing_punctuation, :spacing_punctuation, :left_bracketing_punctuation, :right_bracketing_punctuation], :default     => :word, :null => false
+    change_column :tokens, :sort, :enum, :limit => [:text, :punctuation, :empty_dependency_token, :lacuna_start, :lacuna_end], :default     => :text, :null => false
   end
 end
