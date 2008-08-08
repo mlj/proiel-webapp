f81843c3047cc7f020f36fd0cca24c2d25b81e5f
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2008-08-08 14:30:30 +0200

    Enabled future imperative for Latin.

--- lib/proiel/morphtag_constraints.rb
+++ lib/proiel/morphtag_constraints.rb
@@ -36,16 +36,17 @@ module PROIEL
       :fut_inf =>       /^....fn/,
       :past_part =>     /^....up/,
       :aorist_imp =>    /^....am/,
+      :fut_imp =>       /^....fm/,
     }
 
     # A specification of feature sets that should be treated as invalid
     # in specific languages
     LANGUAGE_BLACK_LISTS = {
       :la =>  [ :art, :dual,             :ins,       :aorist, :resultative, :past,             :optative, :middle, :gen_dat, :fut_inf,                                     :aorist_imp, ],
-      :grc => [       :dual,       :abl, :ins, :loc,          :resultative, :past,                                 :gen_dat,           ],
-      :hy  => [ :art, :dual, :voc, :abl, :ins,                :resultative, :past,             :optative, :middle, :gen_dat, :fut_inf, ],
-      :got => [ :art,              :abl, :ins, :loc, :aorist, :resultative,        :past_part,            :middle, :gen_dat, :fut_inf,                                     :aorist_imp, ],
-      :cu  => [ :art,              :abl,                                                       :optative, :middle,           :fut_inf, :pluperfect, :futperfect, :perfect, :aorist_imp, ],
+      :grc => [       :dual,       :abl, :ins, :loc,          :resultative, :past,                                 :gen_dat,                                                            :fut_imp, ],
+      :hy  => [ :art, :dual, :voc, :abl, :ins,                :resultative, :past,             :optative, :middle, :gen_dat, :fut_inf,                                                  :fut_imp, ],
+      :got => [ :art,              :abl, :ins, :loc, :aorist, :resultative,        :past_part,            :middle, :gen_dat, :fut_inf,                                     :aorist_imp, :fut_imp, ],
+      :cu  => [ :art,              :abl,                                                       :optative, :middle,           :fut_inf, :pluperfect, :futperfect, :perfect, :aorist_imp, :fut_imp, ],
     }
 
     private
