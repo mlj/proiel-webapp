a5d27d5aa525b27607b749ac90cc6d78f307ad35
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2008-08-15 14:16:47 +0200

    Added sentence_controller#destroy and overhauled token relocation functions.

--- app/controllers/sentences_controller.rb
+++ app/controllers/sentences_controller.rb
@@ -1,4 +1,6 @@
 class SentencesController < ApplicationController
+  before_filter :is_reviewer?, :only => [:destroy]
+
   # GET /sentences
   # GET /sentences.xml
   def index
@@ -18,6 +20,37 @@ class SentencesController < ApplicationController
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @sentence }
+    end
+  end
+
+  # DELETE /sentence/1
+  # DELETE /sentence/1.xml
+  #
+  # Destroys the sentence. This works by moving all tokens in the sentence to the
+  # previous sentence in the linear order before actually removing the sentence.
+  # If there is no previous sentence, then destruction will fail.
+  def destroy
+    @sentence = Sentence.find(params[:id])
+
+    if @sentence.has_previous_sentence?
+      previous_sentence = @sentence.previous_sentence
+
+      versioned_transaction do
+        previous_sentence.append_first_tokens_from_next_sentence!(@sentence.tokens.length)
+        @sentence.destroy
+      end
+
+      respond_to do |format|
+        flash[:notice] = 'Successfully destroyed.'
+        format.html { redirect_to(previous_sentence) }
+        format.xml  { head :ok }
+      end
+    else
+      respond_to do |format|
+        flash[:error] = 'Sentence cannot be destroyed.'
+        format.html { redirect_to(@sentence) }
+        format.xml  { render :status => :unprocessable_entity }
+      end
     end
   end
 end
--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -223,19 +223,17 @@ class Sentence < ActiveRecord::Base
     end
   end
 
-  # Append tokens to this sentence by reassigning its sentence and token number
-  # and save the affected record.
-  def append_tokens!(tokens)
-    tokens.each do |t|
+  private
+
+  def append_tokens!(ts) #:nodoc:
+    ts.each do |t|
       t.sentence_id = id 
       t.token_number = self.max_token_number + 1
       t.save!
     end
   end
 
-  # Prepend tokens to this sentence by reassigning sentence and token numbers
-  # and save the affected records.
-  def prepend_tokens!(ts)
+  def prepend_tokens!(ts) #:nodoc:
     m = self.min_token_number 
 
     if m.nil?
@@ -259,8 +257,10 @@ class Sentence < ActiveRecord::Base
     end
   end
 
-  # Reassign the +n+ first token of the next sentence to the end of
-  # this sentence and save the affected record.
+  public
+
+  # Move the +n+ first tokens from the next sentence to the end of
+  # this sentence and save the affected records.
   def append_first_tokens_from_next_sentence!(n = 1)
     if self.has_next_sentence? 
       append_tokens!(self.next_sentence.tokens.first(n))
@@ -269,8 +269,8 @@ class Sentence < ActiveRecord::Base
     end
   end
 
-  # Reassign the +n+ last token of the previous sentence to the
-  # beginning of this sentence and save the affected record.
+  # Move the +n+ last token from the previous sentence to the
+  # beginning of this sentence and save the affected records.
   def prepend_last_tokens_from_previous_sentence!(n = 1)
     if self.has_previous_sentence? 
       prepend_tokens!(self.previous_sentence.tokens.last(n))
