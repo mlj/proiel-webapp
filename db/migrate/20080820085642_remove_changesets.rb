af597ef812d5ccbb7d6ccfc66ce2bbc4f5380d5a
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2008-08-21 14:10:48 +0200

    Fixed broken migration.

--- db/migrate/20080820085642_remove_changesets.rb
+++ db/migrate/20080820085642_remove_changesets.rb
@@ -1,7 +1,7 @@
 class RemoveChangesets < ActiveRecord::Migration
   def self.up
-#    add_column :audits, :user_id, :integer, :null => false
-#    add_column :audits, :created_at, :datetime, :null => false
+    add_column :audits, :user_id, :integer, :null => false
+    add_column :audits, :created_at, :datetime, :null => false
 
     execute("UPDATE audits LEFT JOIN changesets ON changeset_id = changesets.id SET audits.user_id = changesets.user_id")
     execute("UPDATE audits LEFT JOIN changesets ON changeset_id = changesets.id SET audits.created_at = changesets.created_at")
