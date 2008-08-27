f0386210737ca603fb6c1829dcb0c4caa3fcee02
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2008-08-27 11:31:39 +0200

    Moved UNICODE_NORMALIZATION_FORM definition to config/environment so that it is actually usable in models.

--- config/initializers/languages.rb
+++ config/initializers/languages.rb
@@ -12,13 +12,3 @@ TRANSLITERATORS = {
   :got => 'got-ascii-word',
   :grc => 'grc-betacode',
 }
-
-# Unicode settings
-# ================
-
-# Unicode normalization form for normalised text columns. Choices are :kc, :c, :kd, :d
-# (see http://unicode.org/reports/tr15/ for details). The default choice is :c, and it is
-# recommended that you stick with that unless you have good reasons not to. If you do change
-# it, remeber to take into consideration how your database engine deals with queries that
-# contain sequences of potentially decomposed characters.
-UNICODE_NORMALIZATION_FORM = :c
