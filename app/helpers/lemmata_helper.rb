c532de0ec8df13dd59b9df273f4cfa19b24f8ffc
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2008-09-02 11:23:25 +0200

    Cleaned up Lemmata.index and Lemmata.show views.

--- app/helpers/lemmata_helper.rb
+++ app/helpers/lemmata_helper.rb
@@ -1,4 +1,9 @@
 module LemmataHelper
+  # Creates a table view of a collection of lemmata.
+  def lemmata_table(lemmata)
+    render_tabular lemmata, :partial => 'lemmata/lemma', :pagination => true, :fields => [ 'Lemma', 'Language', 'Translation', 'Frequency', '&nbsp;' ]
+  end
+
   # Creates a link to a lemma.
   def link_to_lemma(lemma)
     link_to(lemma.variant ? "#{lemma.lemma}##{lemma.variant}" : lemma.lemma, lemma)
