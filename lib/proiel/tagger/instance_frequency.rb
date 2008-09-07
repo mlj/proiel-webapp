a25e15a87096b190c76c66bf48352fafbfc3ee9f
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2008-09-07 14:47:10 +0200

    Fix: corrected broken instance frequency retrieval.

--- lib/proiel/tagger/instance_frequency.rb
+++ lib/proiel/tagger/instance_frequency.rb
@@ -27,8 +27,8 @@ module PROIEL
       private
 
       def grab_data(form)
-        source = Source.find_by_language(@language)
-        result = Token.connection.select_all("SELECT morphtag, lemmata.lemma, lemmata.variant, count(*) AS frequency FROM tokens LEFT JOIN sentences ON sentence_id = sentences.id LEFT JOIN lemmata ON lemma_id = lemmata.id WHERE form = \"#{form}\" AND source_id = #{source.id} AND reviewed_by IS NOT NULL GROUP BY morphtag, lemma_id", 'Token')
+        language = Language.find_by_iso_code(@language)
+        result = Token.connection.select_all("SELECT morphtag, lemmata.lemma, lemmata.variant, count(*) AS frequency FROM tokens LEFT JOIN sentences ON sentence_id = sentences.id LEFT JOIN lemmata ON lemma_id = lemmata.id WHERE form = \"#{form}\" AND source_id IN (#{language.sources.map(&:id).join(',')}) AND reviewed_by IS NOT NULL GROUP BY morphtag, lemma_id", 'Token')
         result.collect! do |e|
           [e["morphtag"], e["lemma"] ? (e["variant"] ? [e["lemma"], e["variant"]].join('#') : e["lemma"]) : nil, e["frequency"].to_i]
         end
