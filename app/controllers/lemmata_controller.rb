5d0e25919652ae5dc1d81e8f1c82e1a2bf1069a8
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2008-09-09 15:27:18 +0200

    Fix: use abbreviated morphological descriptions in morphology stats for lemmata.

--- app/controllers/lemmata_controller.rb
+++ app/controllers/lemmata_controller.rb
@@ -5,7 +5,7 @@ class LemmataController < ResourceController::Base
   show.before do
     @semantic_tags = @lemma.semantic_tags
     @tokens = @lemma.tokens.search(params[:query], :page => current_page)
-    @morphology_stats = @lemma.tokens.count(:group => :morphtag).map { |k, v| [PROIEL::MorphTag.new(k).descriptions([:major, :minor], false).join(', ').capitalize, v] }
+    @morphology_stats = @lemma.tokens.count(:group => :morphtag).map { |k, v| [PROIEL::MorphTag.new(k).descriptions([:major, :minor], false, :style => :abbreviation).join(', ').capitalize, v] }
     # FIXME: find a better way to skip this if impossible to inflect?
     @morphology_stats = nil if @morphology_stats.length == 1 and @morphology_stats.first.first == ''
   end
