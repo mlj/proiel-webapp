e08699646ab9f5fafa9c35e7ed3a0088befc1d88
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2008-09-09 13:37:10 +0200

    Added simple morphological statistics.

--- app/controllers/lemmata_controller.rb
+++ app/controllers/lemmata_controller.rb
@@ -5,6 +5,9 @@ class LemmataController < ResourceController::Base
   show.before do
     @semantic_tags = @lemma.semantic_tags
     @tokens = @lemma.tokens.search(params[:query], :page => current_page)
+    @morphology_stats = @lemma.tokens.count(:group => :morphtag).map { |k, v| [PROIEL::MorphTag.new(k).descriptions([:major, :minor], false).join(', ').capitalize, v] }
+    # FIXME: find a better way to skip this if impossible to inflect?
+    @morphology_stats = nil if @morphology_stats.length == 1 and @morphology_stats.first.first == ''
   end
 
   update.before do
