4a88f231151da8acf87ee96f53d601f98ecc108e
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2008-11-20 15:57:35 +0000

    Updated to Rails 2.1.2 and fixed resource_controller dependency mess.

--- config/environment.rb
+++ config/environment.rb
@@ -17,7 +17,7 @@ UNICODE_NORMALIZATION_FORM = :c
 DEFAULT_USER_PREFERENCES = { :graph_format => "png", :graph_method => "unsorted" }
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.1.1' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.1.2' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -71,7 +71,9 @@ Rails::Initializer.run do |config|
   # config.active_record.default_timezone = :utc
 
   config.gem 'mislav-will_paginate', :version => '~> 2.3.2', :lib => 'will_paginate', :source => 'http://gems.github.com'
-  config.gem 'resource_controller'
+  # Versions > 0.5.2 of r_c mess with Rails' dependency loading system. Is it
+  # already time to migrate away from r_c?
+  config.gem 'resource_controller', :lib => 'resource_controller', :version => '0.5.2'
   config.gem 'unicode'
   config.gem 'oniguruma'
   config.gem 'builder'
