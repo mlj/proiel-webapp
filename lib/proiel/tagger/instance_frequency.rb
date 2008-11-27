b59a7dd043ab44b06dbd42ee385bb81a60144429
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2008-11-27 23:35:17 +0000

    Fix: broken tagger frequencies due to missing source_divisions join.

--- lib/proiel/tagger/instance_frequency.rb
+++ lib/proiel/tagger/instance_frequency.rb
@@ -28,7 +28,7 @@ module PROIEL
 
       def grab_data(form)
         language = Language.find_by_iso_code(@language)
-        result = Token.connection.select_all("SELECT morphtag, lemmata.lemma, lemmata.variant, count(*) AS frequency FROM tokens LEFT JOIN sentences ON sentence_id = sentences.id LEFT JOIN lemmata ON lemma_id = lemmata.id WHERE form = \"#{form}\" AND source_id IN (#{language.sources.map(&:id).join(',')}) AND reviewed_by IS NOT NULL GROUP BY morphtag, lemma_id", 'Token')
+        result = Token.connection.select_all("SELECT morphtag, lemmata.lemma, lemmata.variant, count(*) AS frequency FROM tokens LEFT JOIN sentences ON sentence_id = sentences.id LEFT JOIN source_divisions ON source_division_id = source_divisions.id LEFT JOIN lemmata ON lemma_id = lemmata.id WHERE form = \"#{form}\" AND source_id IN (#{language.sources.map(&:id).join(',')}) AND reviewed_by IS NOT NULL GROUP BY morphtag, lemma_id", 'Token')
         result.collect! do |e|
           [e["morphtag"], e["lemma"] ? (e["variant"] ? [e["lemma"], e["variant"]].join('#') : e["lemma"]) : nil, e["frequency"].to_i]
         end
