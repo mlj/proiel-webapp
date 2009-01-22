70bee558b7295d9f985502cddec9f16b51195399
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2009-01-22 08:29:54 +0000

    Fix: broken "spam list" patch.

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -1,3 +1,3 @@
-ExceptionNotifier.exception_recipients = %w(mariuslj@ifi.uio.no)
+ExceptionNotifier.exception_recipients = %w(mariuslj@ifi.uio.no d.t.t.haug@ifikk.uio.no)
 #ExceptionNotifier.sender_address =  %("Name Error <error@appname.url>")
 #ExceptionNotifier.email_prefix = "[App name Exception] "
