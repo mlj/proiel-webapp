66564aaa577c42b6eeab757ca7272bc1a798ad20
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2009-03-22 23:07:25 +0000

    Fix: fixed migration MySQL 4 compatibility problem

--- db/migrate/20090320204246_fix_non_numeric_chapter_references.rb
+++ db/migrate/20090320204246_fix_non_numeric_chapter_references.rb
@@ -1,23 +1,23 @@
 class FixNonNumericChapterReferences < ActiveRecord::Migration
   def self.up
     ['fields'].each do |field|
-      execute("UPDATE source_divisions SET #{field} = replace(#{field}, '=0', '=Incipit') WHERE #{field} LIKE '%=0'");
-      execute("UPDATE source_divisions SET #{field} = replace(#{field}, '=255', '=Explicit') WHERE #{field} LIKE '%=255'");
+      execute("UPDATE source_divisions SET source_divisions.#{field} = replace(source_divisions.#{field}, '=0', '=Incipit') WHERE source_divisions.#{field} LIKE '%=0'");
+      execute("UPDATE source_divisions SET source_divisions.#{field} = replace(source_divisions.#{field}, '=255', '=Explicit') WHERE source_divisions.#{field} LIKE '%=255'");
     end
     ['title', 'abbreviated_title'].each do |field|
-      execute("UPDATE source_divisions SET #{field} = replace(#{field}, ' 0', ' Incipit') WHERE #{field} LIKE '% 0'");
-      execute("UPDATE source_divisions SET #{field} = replace(#{field}, ' 255', ' Explicit') WHERE #{field} LIKE '% 255'");
+      execute("UPDATE source_divisions SET source_divisions.#{field} = replace(source_divisions.#{field}, ' 0', ' Incipit') WHERE source_divisions.#{field} LIKE '% 0'");
+      execute("UPDATE source_divisions SET source_divisions.#{field} = replace(source_divisions.#{field}, ' 255', ' Explicit') WHERE source_divisions.#{field} LIKE '% 255'");
     end
   end
 
   def self.down
     ['fields'].each do |field|
-      execute("UPDATE source_divisions SET #{field} = replace(#{field}, '=Incipit', '=0') WHERE #{field} LIKE '%=Incipit'");
-      execute("UPDATE source_divisions SET #{field} = replace(#{field}, '=Explicit', '=255') WHERE #{field} LIKE '%=Explicit'");
+      execute("UPDATE source_divisions SET source_divisions.#{field} = replace(source_divisions.#{field}, '=Incipit', '=0') WHERE source_divisions.#{field} LIKE '%=Incipit'");
+      execute("UPDATE source_divisions SET source_divisions.#{field} = replace(source_divisions.#{field}, '=Explicit', '=255') WHERE source_divisions.#{field} LIKE '%=Explicit'");
     end
     ['title', 'abbreviated_title'].each do |field|
-      execute("UPDATE source_divisions SET #{field} = replace(#{field}, ' Incipit', ' 0') WHERE #{field} LIKE '% Incipit'");
-      execute("UPDATE source_divisions SET #{field} = replace(#{field}, ' Explicit', ' 255') WHERE #{field} LIKE '% Explicit'");
+      execute("UPDATE source_divisions SET source_divisions.#{field} = replace(source_divisions.#{field}, ' Incipit', ' 0') WHERE source_divisions.#{field} LIKE '% Incipit'");
+      execute("UPDATE source_divisions SET source_divisions.#{field} = replace(source_divisions.#{field}, ' Explicit', ' 255') WHERE source_divisions.#{field} LIKE '% Explicit'");
     end
   end
 end
