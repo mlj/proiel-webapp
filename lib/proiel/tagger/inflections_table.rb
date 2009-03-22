92debad6731a5c9905b7e1d33451efcea5c86274
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2009-03-22 21:18:50 +0000

    Fix: corrected funny invalidation of Language object in tagger.

--- lib/proiel/tagger/inflections_table.rb
+++ lib/proiel/tagger/inflections_table.rb
@@ -9,12 +9,13 @@ module PROIEL
     class InflectionsTableMethod < TaggerAnalysisMethod
       def initialize(language)
         super(language)
-
-        @language = Language.find_by_iso_code(language)
       end
 
       def analyze(form)
-        @language.inflections.find_all_by_form(form).collect do |instance|
+        language = Language.find_by_iso_code(@language)
+        raise "invalid language #{@language}" unless language
+
+        language.inflections.find_all_by_form(form).collect do |instance|
           [PROIEL::MorphLemmaTag.new("#{instance.morphtag}:#{instance.lemma}"), 1]
         end
       end
