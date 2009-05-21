79a69a28f57fffed5df7449c4bce96109e9d5d67
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2009-05-21 12:43:56 +0100

    Fix: added missing migration.

---
+++ db/migrate/20090521104823_add_rules_to_inflections.rb
@@ -0,0 +1,31 @@
+class AddRulesToInflections < ActiveRecord::Migration
+  def self.up
+  #  add_column :inflections, :manual_rule, :boolean, :default => false, :null => false
+
+    %W{lat got grc chu}.each do |language_code|
+      language = Language.find_by_iso_code(language_code)
+
+      File.open("db/migrate/#{language_code}-rules.csv") do |f|
+        f.each_line do |l|
+          l.chomp!
+          dummy_language_code, base_form, variant, form, morphtag = l.split(',')
+          lemma = variant.blank? ? base_form : [base_form, variant].join('#')
+          morphtag = morphtag.ljust(13, '-')
+
+          if o = language.inflections.find_by_form_and_morphtag_and_lemma(form, morphtag, lemma)
+            STDERR.puts "Upgrading an existing inflection to a manual rule" unless o.manual_rule
+            o.manual_rule = true
+            o.save!
+          else
+            language.inflections.create! :form => form, :morphtag => morphtag, :lemma => lemma,
+              :manual_rule => true
+          end
+        end
+      end
+    end
+  end
+
+  def self.down
+    remove_column :inflections, :manual_rule
+  end
+end
