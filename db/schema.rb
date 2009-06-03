a54e142a367d0a60bf030e2080e5f1d983dbb7f1
Dag Haug <daghaug@ifikk.uio.no>
2009-06-03 16:34:17 +0200

    Added old-inactive tag to tokens.info_status.
    Signed-off-by: Marius L. Jøhndal <mariuslj@ifi.uio.no>

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090523133728) do
+ActiveRecord::Schema.define(:version => 20090531145517) do
 
   create_table "announcements", :force => true do |t|
     t.text     "message"
@@ -244,9 +244,9 @@ ActiveRecord::Schema.define(:version => 20090523133728) do
   end
 
   create_table "tokens", :force => true do |t|
-    t.integer  "sentence_id",                                                                                                                     :default => 0,     :null => false
+    t.integer  "sentence_id",                                                                                                                                 :default => 0,     :null => false
     t.integer  "verse"
-    t.integer  "token_number",                                                                                                                    :default => 0,     :null => false
+    t.integer  "token_number",                                                                                                                                :default => 0,     :null => false
     t.string   "form",                         :limit => 64
     t.integer  "lemma_id"
     t.integer  "head_id"
@@ -256,20 +256,20 @@ ActiveRecord::Schema.define(:version => 20090523133728) do
     t.string   "source_morphology",            :limit => 17
     t.string   "source_lemma",                 :limit => 32
     t.text     "foreign_ids"
-    t.boolean  "contraction",                                                                                                                     :default => false, :null => false
+    t.boolean  "contraction",                                                                                                                                 :default => false, :null => false
     t.enum     "nospacing",                    :limit => [:before, :after, :both]
     t.string   "presentation_form",            :limit => 128
     t.integer  "presentation_span"
-    t.boolean  "emendation",                                                                                                                      :default => false, :null => false
-    t.boolean  "abbreviation",                                                                                                                    :default => false, :null => false
-    t.boolean  "capitalisation",                                                                                                                  :default => false, :null => false
-    t.enum     "info_status",                  :limit => [:new, :acc, :acc_gen, :acc_disc, :acc_inf, :old, :no_info_status, :info_unannotatable]
+    t.boolean  "emendation",                                                                                                                                  :default => false, :null => false
+    t.boolean  "abbreviation",                                                                                                                                :default => false, :null => false
+    t.boolean  "capitalisation",                                                                                                                              :default => false, :null => false
+    t.enum     "info_status",                  :limit => [:new, :acc, :acc_gen, :acc_disc, :acc_inf, :old, :old_inact, :no_info_status, :info_unannotatable]
     t.string   "empty_token_sort",             :limit => 1
     t.string   "contrast_group"
     t.integer  "antecedent_dist_in_words"
     t.integer  "antecedent_dist_in_sentences"
     t.integer  "token_alignment_id"
-    t.boolean  "automatic_token_alignment",                                                                                                       :default => false
+    t.boolean  "automatic_token_alignment",                                                                                                                   :default => false
     t.integer  "dependency_alignment_id"
     t.integer  "antecedent_id"
     t.integer  "relation_id"
