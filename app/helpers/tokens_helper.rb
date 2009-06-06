b65e5a17fb678daed0d021523d70e8560fd40f82
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2009-06-06 14:09:53 +0100

    Fix: removed obsoleted readable_presentation_flags.

--- app/helpers/tokens_helper.rb
+++ app/helpers/tokens_helper.rb
@@ -8,12 +8,4 @@ module TokensHelper
   def link_to_token(token)
     link_to "Token #{token.id}", token
   end
-
-  def readable_presentation_flags(t)
-    flags = []
-    [:contraction, :emendation, :abbreviation, :capitalisation].each do |flag|
-      flags << flag.to_s if t.send(flag)
-    end
-    flags.join(', ')
-  end
 end
