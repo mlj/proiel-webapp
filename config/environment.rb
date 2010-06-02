a153ecc6653a5421d60174687e21644cf7259340
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2010-06-02 15:28:30 +0100

    Upgraded to Rails 2.3.8.

--- config/environment.rb
+++ config/environment.rb
@@ -16,8 +16,7 @@ UNICODE_NORMALIZATION_FORM = :c
 # Default user preferences.
 DEFAULT_USER_PREFERENCES = { :graph_format => "png", :graph_method => "unsorted" }
 
-# Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
