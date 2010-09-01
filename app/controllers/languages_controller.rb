147508126c4a583851b831c1cf9499ee68107471
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2010-09-01 06:37:24 +0100

    Removed administrative views for Language model.

--- app/controllers/languages_controller.rb
+++
@@ -1,9 +0,0 @@
-class LanguagesController < InheritedResources::Base
-  actions :index, :show
-
-  private
-
-  def collection
-    @languages = Language.search(params[:q], :page => params[:page])
-  end
-end
