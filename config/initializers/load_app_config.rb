1291c60afcc1aa7417f2f09036cc07a1b3f134a2
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2010-11-19 22:00:14 +0100

    Made application config object (slightly) easier to access.

--- config/initializers/load_app_config.rb
+++ config/initializers/load_app_config.rb
@@ -1,4 +1,16 @@
-app_config = YAML.load_file(Rails.root.join('config', 'app_config.yml'))
-CONFIG = (app_config["all"] || {}).symbolize_keys
-CONFIG.merge!((app_config[Rails.env] || {}).symbolize_keys)
-CONFIG.freeze
+CONFIG = Class.new do
+  def initialize(file_name)
+    app_config = YAML.load_file(Rails.root.join('config', 'app_config.yml'))
+    @d = (app_config["all"] || {}).symbolize_keys
+    @d.merge!((app_config[Rails.env] || {}).symbolize_keys)
+    @d.freeze
+  end
+
+  def method_missing(name, *args)
+    if @d.has_key?(name)
+      @d[name]
+    else
+      super
+    end
+  end
+end.new(Rails.root.join('config', 'app_config.yml'))
