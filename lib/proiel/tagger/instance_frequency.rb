658aba8316582098c5f853c67ce739a9f7e63993
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2011-04-07 22:42:33 +0100

    Fix: make SQL query for instance-frequency tagging less dependent on MySQL behaviour.

--- lib/proiel/tagger/instance_frequency.rb
+++ lib/proiel/tagger/instance_frequency.rb
@@ -1,9 +1,24 @@
-#!/usr/bin/env ruby
+#--
 #
-# instance_frequency.rb - Morphological tagger: Instance frequency method
+# Copyright 2008, 2009, 2010, 2011 University of Oslo
+# Copyright 2008, 2009, 2010, 2011 Marius L. Jøhndal
 #
-# Written by Marius L. Jøhndal, 2008.
+# This file is part of the PROIEL web application.
 #
+# The PROIEL web application is free software: you can redistribute it
+# and/or modify it under the terms of the GNU General Public License
+# version 2 as published by the Free Software Foundation.
+#
+# The PROIEL web application is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with the PROIEL web application.  If not, see
+# <http://www.gnu.org/licenses/>.
+#
+#++
 module Tagger
   class InstanceFrequencyMethod < TaggerAnalysisMethod
     def initialize(language)
@@ -11,7 +26,8 @@ module Tagger
     end
 
     def analyze(form)
-      x = Token.connection.select_all("SELECT tokens.id AS token_id, count(*) AS frequency FROM tokens LEFT JOIN sentences ON sentence_id = sentences.id LEFT JOIN lemmata ON lemma_id = lemmata.id WHERE form = #{Token.sanitize(form)} AND lemmata.language = #{Token.sanitize(@language.to_s)} AND reviewed_by IS NOT NULL GROUP BY morphology, lemma_id", 'Token')
+      # Keep MIN(tokens.id) in the SELECT-clause to apease Oracle et al.
+      x = Token.connection.select_all("SELECT MIN(tokens.id) AS token_id, count(*) AS frequency FROM tokens LEFT JOIN sentences ON sentence_id = sentences.id LEFT JOIN lemmata ON lemma_id = lemmata.id WHERE form = #{Token.sanitize(form)} AND lemmata.language = #{Token.sanitize(@language.to_s)} AND reviewed_by IS NOT NULL GROUP BY morphology, lemma_id", 'Token')
       sum = x.map { |i| i["frequency"].to_i }.sum.to_f
       x.map { |i| [Token.find(i["token_id"]).morph_features, i["frequency"].to_i / sum] }
     end
