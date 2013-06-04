97e392209a2717540f8a4805a4962080f2ef4067
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2013-06-04 21:15:17 +0100

    Put secret token and exception notification config in .env and provide fall-back values if missing.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,16 +5,10 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 
-secret_token_file = Rails.root.join('config', 'secret_token')
-
-if File.exists?(secret_token_file)
-  Proiel::Application.config.secret_token = File.open(secret_token_file).read
-else
-  puts "Missing secret token file #{secret_token_file}"
-  puts
-  puts "*********************************************************************".red
-  puts "Read step 6 of the installation guide (doc/guide.mkd) if this message".red
-  puts "doesn't make sense to you!".red
-  puts "*********************************************************************".red
-  exit
+if ENV['PROIEL_SECRET_TOKEN']
+  Proiel::Application.config.secret_token = ENV['PROIEL_SECRET_TOKEN']
+elsif Rails.env.development? or Rails.env.test?
+  puts 'Warning: Generating random secret token'.yellow
+  Rails.logger.warn 'Generating random secret token'
+  Proiel::Application.config.secret_token = SecureRandom.hex(64)
 end
