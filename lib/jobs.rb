7a905effa79cc199867c27dea5d143215e0e70ef
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2013-09-12 16:09:50 +0100

    Fix: added missing lib/jobs.rb.

---
+++ lib/jobs.rb
@@ -0,0 +1,45 @@
+# encoding: UTF-8
+#--
+#
+# Copyright 2007, 2008, 2009, 2010, 2011, 2012, 2013 University of Oslo
+# Copyright 2007, 2008, 2009, 2010, 2011, 2012, 2013 Marius L. Jøhndal
+#
+# This file is part of the PROIEL web application.
+#
+# The PROIEL web application is free software: you can redistribute it
+# and/or modify it under the terms of the GNU General Public License
+# version 2 as published by the Free Software Foundation.
+#
+# The PROIEL web application is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with the PROIEL web application.  If not, see
+# <http://www.gnu.org/licenses/>.
+#
+#++
+
+module Proiel
+  module Jobs
+    class Job
+      def initialize(logger = Rails.logger)
+        @logger = logger
+      end
+
+      def run_periodically!(interval)
+        while true do
+          @logger.info { "Running #{self.class}" }
+          run_once!
+
+          @logger.info { "Sleeping #{self.class}" }
+          sleep interval
+        end
+      end
+
+      def run_once!
+      end
+    end
+  end
+end
