24cc9bbdc6e70cf38dc83c60e6aba9219e126c71
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2013-12-13 23:02:18 +0000

    Fix: SemanticTag#semantic_attribute_value should be accessible.

--- app/models/semantic_tag.rb
+++ app/models/semantic_tag.rb
@@ -1,4 +1,29 @@
+# encoding: UTF-8
+#--
+#
+# Copyright 2007, 2008, 2009, 2010, 2011, 2012, 2013 University of Oslo
+# Copyright 2007, 2008, 2009, 2010, 2011, 2012, 2013 Marius L. Jøhndal
+#
+# This file is part of the PROIEL web application.
+#
+# The PROIEL web application is free software: you can redistribute it
+# and/or modify it under the terms of the GNU General Public License
+# version 2 as published by the Free Software Foundation.
+#
+# The PROIEL web application is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with the PROIEL web application.  If not, see
+# <http://www.gnu.org/licenses/>.
+#
+#++
+
 class SemanticTag < ActiveRecord::Base
+  attr_accessible :semantic_attribute_value
+
   belongs_to :semantic_attribute_value
   belongs_to :taggable, :polymorphic => true
 
