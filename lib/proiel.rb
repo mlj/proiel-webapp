b63f423b024729a41a9ba8c1dd55a760470cd319
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2013-12-16 18:59:33 +0000

    Bumped version number to 1.2.1.

--- lib/proiel.rb
+++ lib/proiel.rb
@@ -40,7 +40,7 @@ autoload :Metadata, 'metadata'
 
 module Proiel
   # The application version
-  VERSION = [1, 2, 0]
+  VERSION = [1, 2, 1]
 
   # Returns the application version as a dotted string.
   def self.version
