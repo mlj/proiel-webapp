9bb108f23c0269c26918d658d562ec43fc2119aa
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2014-01-19 00:33:58 +0000

    Redid sources#show using Semantic UI.

--- app/helpers/sources_helper.rb
+++ app/helpers/sources_helper.rb
@@ -3,4 +3,15 @@ module SourcesHelper
   def link_to_source(source)
     link_to source.title, source
   end
+
+  def metadata_fields_and_labels(source)
+    {
+      'Treebank' => Proiel::Metadata::treebank_fields_and_labels,
+      'Electronic text' => Proiel::Metadata::electronic_text_fields_and_labels,
+      'Printed text' => Proiel::Metadata::printed_text_fields_and_labels,
+    }.map do |header, data|
+      d = data.map { |(f, lbl)| [lbl, @source.send(f)] }.reject { |(_, f)| f.blank? }
+      [header, d]
+    end
+  end
 end
