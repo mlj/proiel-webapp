eba999759c08710d881e77a3d467940c5280ed65
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2016-02-13 15:43:12 +0000

    Bump version number to 1.7.0.

--- lib/lib_files.rb
+++ lib/lib_files.rb
@@ -1,8 +1,8 @@
 # encoding: UTF-8
 #--
 #
-# Copyright 2013, 2015 University of Oslo
-# Copyright 2013, 2014, 2015 Marius L. Jøhndal
+# Copyright 2013-2016 University of Oslo
+# Copyright 2013-2016 Marius L. Jøhndal
 #
 # This file is part of the PROIEL web application.
 #
@@ -32,7 +32,7 @@ autoload :BiblosExternalLinkMapper, 'external_link_mapper'
 
 module Proiel
   # The application version
-  VERSION = [1, 6, 0]
+  VERSION = [1, 7, 0]
 
   # Returns the application version as a dotted string.
   def self.version
