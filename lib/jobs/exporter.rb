mlj/proiel-webapp

commit 99f00a2407af36e8c2e94e368a3d5cdb7fd0da98

Remove export job.
This removes the export job which has until now been automatically
started by foreman. These jobs over time get so heavy and
time-consuming that they can overtake each other and cause massive
system load. To avoid this they should instead be run with proper
supervision and independently of the server processes.

lib/jobs/exporter.rb
@@ -1,69 +1,0 @@
-# encoding: UTF-8
-#--
-#
-# Copyright 2007-2013 University of Oslo
-# Copyright 2007-2016 Marius L. Jøhndal
-#
-# This file is part of the PROIEL web application.
-#
-# The PROIEL web application is free software: you can redistribute it
-# and/or modify it under the terms of the GNU General Public License
-# version 2 as published by the Free Software Foundation.
-#
-# The PROIEL web application is distributed in the hope that it will be
-# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with the PROIEL web application.  If not, see
-# <http://www.gnu.org/licenses/>.
-#
-#++
-
-module Proiel
-  module Jobs
-    class Exporter < Job
-      # ==== Options
-      # * <tt>:id</tt> - If set, only exports the source with the given ID. If not set,
-      # all sources are expored.
-      # * <tt>:source_division</tt> - If set to a regular expression, only
-      # exports source divisions whose titles match the regular expression.
-      # * <tt>:semantic_tags</tt> - If true, will also export semantic tags.
-      def initialize(export_directory, logger = Rails.logger, options = {})
-        raise ArgumentError, "no such directory #{@export_directory}" unless export_directory and File::directory?(export_directory)
-
-        super(logger)
-
-        @export_directory = export_directory
-
-        @options = options
-        @options.symbolize_keys!
-        @options.reverse_merge! semantic_tags: false
-      end
-
-      def run_once!
-        options = {}
-        options[:sem_tags] = true if @options[:semantic_tags]
-
-        # Find sources and iterate them
-        sources = @options[:id] ? Source.find_all_by_id(@options[:id]) : Source.all
-
-        sources.each do |source|
-          file_name = File.join(@export_directory, "#{source.human_readable_id}.xml")
-
-          if @options[:source_division]
-            options[:source_division] = source.source_divisions.select { |sd| sd.title =~ Regexp.new(@options[:source_division]) }.map(&:id)
-          end
-
-          begin
-            @logger.info { "#{self.class}: Exporting source ID #{source.id} as #{file_name}" }
-            PROIELXMLExporter.new(source, options).write(file_name)
-          rescue Exception => e
-            @logger.error { "#{self.class}: Error exporting text #{source.human_readable_id}: #{e}\n" + e.backtrace.join("\n") }
-          end
-        end
-      end
-    end
-  end
-end

lib/lib_files.rb
@@ -47,7 +47,6 @@ module Proiel
     autoload :DatabaseChecker, 'jobs/database_checker'
     autoload :DatabaseValidator, 'jobs/database_validator'
     autoload :AnnotationValidator, 'jobs/annotation_validator'
-    autoload :Exporter, 'jobs/exporter'
   end
 
   autoload :Metadata, 'metadata'

Procfile
@@ -1,3 +1,2 @@
 web: bundle exec unicorn_rails -p $PORT
-export_worker: bundle exec rake run_export_job
 check_worker: bundle exec rake run_checker_job

commit 594901406d5f96ac7b3ec61e063a1c50cc129e0a

tasks: Fix removed export job
This fixes commit 99f00a2407af36e8c2e94e368a3d5cdb7fd0da98 which
inadvertently removed the entire export job functionality. This instead
correctly removes the scheduled job.

lib/jobs/exporter.rb
@@ -1,0 +1,69 @@
+# encoding: UTF-8
+#--
+#
+# Copyright 2007-2013 University of Oslo
+# Copyright 2007-2016 Marius L. Jøhndal
+#
+# This file is part of the PROIEL web application.
+#
+# The PROIEL web application is free software: you can redistribute it
+# and/or modify it under the terms of the GNU General Public License
+# version 2 as published by the Free Software Foundation.
+#
+# The PROIEL web application is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with the PROIEL web application.  If not, see
+# <http://www.gnu.org/licenses/>.
+#
+#++
+
+module Proiel
+  module Jobs
+    class Exporter < Job
+      # ==== Options
+      # * <tt>:id</tt> - If set, only exports the source with the given ID. If not set,
+      # all sources are expored.
+      # * <tt>:source_division</tt> - If set to a regular expression, only
+      # exports source divisions whose titles match the regular expression.
+      # * <tt>:semantic_tags</tt> - If true, will also export semantic tags.
+      def initialize(export_directory, logger = Rails.logger, options = {})
+        raise ArgumentError, "no such directory #{@export_directory}" unless export_directory and File::directory?(export_directory)
+
+        super(logger)
+
+        @export_directory = export_directory
+
+        @options = options
+        @options.symbolize_keys!
+        @options.reverse_merge! semantic_tags: false
+      end
+
+      def run_once!
+        options = {}
+        options[:sem_tags] = true if @options[:semantic_tags]
+
+        # Find sources and iterate them
+        sources = @options[:id] ? Source.find_all_by_id(@options[:id]) : Source.all
+
+        sources.each do |source|
+          file_name = File.join(@export_directory, "#{source.human_readable_id}.xml")
+
+          if @options[:source_division]
+            options[:source_division] = source.source_divisions.select { |sd| sd.title =~ Regexp.new(@options[:source_division]) }.map(&:id)
+          end
+
+          begin
+            @logger.info { "#{self.class}: Exporting source ID #{source.id} as #{file_name}" }
+            PROIELXMLExporter.new(source, options).write(file_name)
+          rescue Exception => e
+            @logger.error { "#{self.class}: Error exporting text #{source.human_readable_id}: #{e}\n" + e.backtrace.join("\n") }
+          end
+        end
+      end
+    end
+  end
+end

lib/lib_files.rb
@@ -42,6 +42,7 @@ module Proiel
     autoload :DatabaseChecker, 'jobs/database_checker'
     autoload :DatabaseValidator, 'jobs/database_validator'
     autoload :AnnotationValidator, 'jobs/annotation_validator'
+    autoload :Exporter, 'jobs/exporter'
   end
 
   autoload :Metadata, 'metadata'

lib/tasks/proiel.rake
@@ -6,12 +6,6 @@ task :run_checker_job => :environment do
   database_checker.run_periodically!(1.hour)
 end
 
-desc "Periodically run the exporter job"
-task :run_export_job => :environment do
-  exporter = Proiel::Jobs::Exporter.new Rails.logger
-  exporter.run_periodically!(1.week)
-end
-
 namespace :proiel do
   task(:myenvironment => :environment) do
   end