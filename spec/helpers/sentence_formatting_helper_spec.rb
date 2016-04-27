66d1309e9dc7e0236f3bda343d9849fc691443db
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2016-04-27 20:12:34 +0100

    Fix broken commit b7d320c6c53113d1a9b23e8a13a5a426769f2887
    This adds changes to the test suite that should have been in the commit.

--- spec/helpers/sentence_formatting_helper_spec.rb
+++ spec/helpers/sentence_formatting_helper_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe SentenceFormattingHelper, type: :helper do
   describe 'TokenText' do
     it "returns HTML for PUA italic/sub/sup characters" do
-      expect(TokenText.token_form_as_html("\u{F0002}GIŠ\u{F0102}GU.ZA")).to eq "<sup>GIŠ</sup>GU.ZA"
-      expect(TokenText.token_form_as_html("\u{F0000}A-BI-IA\u{F0100}")).to eq "<i>A-BI-IA</i>"
-      expect(TokenText.token_form_as_html("\u{F0002}m\u{F0102}\u{F0000}Ar-nu-an-da-an\u{F0100}")).to eq "<sup>m</sup><i>Ar-nu-an-da-an</i>"
-      expect(TokenText.token_form_as_html("\u{F0002}URU\u{F0102}Ka\u{F0001}4\u{F0101}-aš-ka\u{F0001}4\u{F0101}-ma")).to eq "<sup>URU</sup>Ka<sub>4</sub>-aš-ka<sub>4</sub>-ma"
+      expect(TokenText.token_form_as_html("\u{F002}GIŠ\u{F102}GU.ZA")).to eq "<sup>GIŠ</sup>GU.ZA"
+      expect(TokenText.token_form_as_html("\u{F000}A-BI-IA\u{F100}")).to eq "<i>A-BI-IA</i>"
+      expect(TokenText.token_form_as_html("\u{F002}m\u{F102}\u{F000}Ar-nu-an-da-an\u{F100}")).to eq "<sup>m</sup><i>Ar-nu-an-da-an</i>"
+      expect(TokenText.token_form_as_html("\u{F002}URU\u{F102}Ka\u{F001}4\u{F101}-aš-ka\u{F001}4\u{F101}-ma")).to eq "<sup>URU</sup>Ka<sub>4</sub>-aš-ka<sub>4</sub>-ma"
     end
 
     it "returns HTML with line and paragraph breaks" do
