ed695819f01725ba2eb886b46118fecb2aa79875
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2016-04-29 10:05:47 +0100

    Fix: Work around sentences with missing annotated_at or reviewed_at

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -37,10 +37,10 @@ class SourcesController < ApplicationController
     @source_divisions = @source.source_divisions.order(:position).page(current_page).per(300)
 
     sentences = @source.sentences
-    @activity_stats = sentences.annotated.group("DATE_FORMAT(annotated_at, '%Y-%m-%d')").order('annotated_at DESC').limit(10).count
+    @activity_stats = sentences.annotated.where('annotated_at IS NOT NULL').group("DATE_FORMAT(annotated_at, '%Y-%m-%d')").order('annotated_at DESC').limit(10).count
     @sentence_completion_stats = @source.aggregated_status_statistics
-    @annotated_by_stats = sentences.annotated.count(group: :annotated_by).map { |k, v| [User.where(id: k).first.try(:full_name), v] }
-    @reviewed_by_stats = sentences.reviewed.count(group: :reviewed_by).map { |k, v| [User.where(id: k).first.try(:full_name), v] }
+    @annotated_by_stats = sentences.annotated.where('annotated_by IS NOT NULL').group(:annotator).count.map { |k, v| [k.full_name, v] }
+    @reviewed_by_stats = sentences.reviewed.where('reviewed_by IS NOT NULL').group(:reviewer).count.map { |k, v| [k.full_name, v] }
 
     respond_with @source
   end
