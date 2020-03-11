a8affee5dcc842da2393d418e626bff4ce6b5d1c
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2020-03-11 18:17:38 +0000

    Bump version number to 1.9.4

--- lib/lib_files.rb
+++ lib/lib_files.rb
@@ -32,7 +32,7 @@ autoload :BiblosExternalLinkMapper, 'external_link_mapper'
 
 module Proiel
   # The application version
-  VERSION = '1.9.3'
+  VERSION = '1.9.4'
 
   autoload :TokenAnnotationValidator, 'annotation_validator'
   autoload :SentenceAnnotationValidator, 'annotation_validator'
