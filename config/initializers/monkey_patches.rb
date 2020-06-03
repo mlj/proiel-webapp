d7a148a0db756b4166f8735c0efc79ee78ae5d77
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2020-06-03 17:18:43 +0100

    Monkey patch active_support library for Ruby 2.4

---
+++ config/initializers/monkey_patches.rb
@@ -0,0 +1,34 @@
+# Monkey patch active_support 3.2.22 for Ruby => 2.4.
+# https://stackoverflow.com/questions/41703128/error-while-trying-to-load-the-gem-devise-activesupport-duration-cant-be-coe
+
+# pulled from https://github.com/rails/rails/blob/v3.2.22.5/activesupport/lib/active_support/core_ext/numeric/time.rb
+class Numeric
+  def days
+    ActiveSupport::Duration.new(24.hours * self, [[:days, self]])
+  end
+  alias :day :days
+
+  def weeks
+    ActiveSupport::Duration.new(7.days * self, [[:days, self * 7]])
+  end
+  alias :week :weeks
+
+  def fortnights
+    ActiveSupport::Duration.new(2.weeks * self, [[:days, self * 14]])
+  end
+  alias :fortnight :fortnights
+end
+
+# pulled from https://github.com/rails/rails/blob/v3.2.22.5/activesupport/lib/active_support/core_ext/integer/time.rb
+
+class Integer
+  def months
+    ActiveSupport::Duration.new(30.days * self, [[:months, self]])
+  end
+  alias :month :months
+
+  def years
+    ActiveSupport::Duration.new(365.25.days * self, [[:years, self]])
+  end
+  alias :year :years
+end
