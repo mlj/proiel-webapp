a705c41720ef6c728b9b4773d1ca1a4d571ff9d7
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2020-06-11 20:48:46 +0100

    Delete old migrations
    These changes have already been applied in 1.9-branch (at least as of
    rel-1.9.4), so they are not needed in order to support a migration path
    from 1.9-branch to master.

--- db/migrate/20160315175455_add_cached_citation_to_source_divisions.rb
+++
@@ -1,5 +0,0 @@
-class AddCachedCitationToSourceDivisions < ActiveRecord::Migration
-  def change
-    add_column :source_divisions, :cached_citation, :string, limit: 128
-  end
-end
--- db/migrate/20160315182200_add_cached_has_discourse_annotation_to_source_divisions.rb
+++
@@ -1,5 +0,0 @@
-class AddCachedHasDiscourseAnnotationToSourceDivisions < ActiveRecord::Migration
-  def change
-    add_column :source_divisions, :cached_has_discourse_annotation, :boolean
-  end
-end
